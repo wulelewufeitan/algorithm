00c719a39272efedb4c2d7cf1899390eab9526c0
kailin <2285858990@qq.com>
2025-05-29 17:58:28 +0800

    test[算法学习]KMP算法例子

---
+++ src/main/java/com/wll/category/string/Kmp.java
@@ -0,0 +1,16 @@
+package com.wll.category.string;
+
+/**
+ * 作者: kalin
+ * 创建时间: 2025/5/29 17时 57分
+ */
+public class Kmp {
+
+    public static void main(String[] args) {
+
+    }
+
+    private int[] lps(String s){
+        return new int[s.length()];
+    }
+}
